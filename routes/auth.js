b22eec9c5c4ee1bb306d4138e684c36e29ec8018
Prabhat Kumar <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-08-19 18:19:55 +0000

    Jwt token sign setup

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,11 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+
+
+dotenv.config()// receving files form .env
 
 // using express router to Create a user on url "localhost:3000/api/auth/" and logged in doesnot require
 router.post('/createuser', [
@@ -22,17 +27,32 @@ router.post('/createuser', [
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
         // creating the user in dataBase
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        }).then(user => res.json(user))
-            // cathing the error if email is same as we have index  on email
-            .catch(error => {
-                if (error.code === 11000) {
-                    res.status(400).json({ error: "User with this email Already exists" })
-                }
+        try {
+            const user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
             })
+            // Creating JWT token with user id as the data
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const jwtToken = jwt.sign(data, process.env.JWT_PVT_KEY)
+
+            // return the jwt token as  response
+            res.json({ jwtToken })
+            // cathing the error if email is same as we have index  on email
+        }
+        // catching duplicate email 
+        catch (error) {
+            if (error.code === 11000) {
+                res.status(400).json({ error: "User with this email Already exists" })
+            }
+            res.status(500).json({ error: error.message })
+        }
+        //catching any other error
     } catch (error) {
         console.error(error.message);
         res.status(500).json({ error: "Something went wrong" })
