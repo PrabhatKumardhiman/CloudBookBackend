b40e884643fd89297ff5f812602617f7ca4d384c
Prabhat Kumar <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-08-21 18:32:14 +0000

    Backend Complete

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+var cors = require('cors')
+
 //function exports
 const connectToMongo = require('./connectToMongo');
-
 connectToMongo()// functiont to connect with MongoDB
+
 const app = express()
 
 // to make json request we will use this middleware
 app.use(express.json())
+app.use(cors())
 
 // app.get('/',(req,res)=>{
 //     res.send("Hello World!")
@@ -20,6 +23,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 //app is listening on port number 3000
-app.listen(3000,()=>{
-    console.log("example app listening on http://localhost:"+3000);
+app.listen(5000,()=>{
+    console.log("CloudBook Backend listening on http://localhost:"+5000);
 })
